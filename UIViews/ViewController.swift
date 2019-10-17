24b88b5b0de297468eeaf4afc597707ddf0c88ca
Jarrale Butts <jbutts1234@gmail.com>
2019-10-17 19:19:06 -0400

    Initial Commit

---
+++ UIViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViews
+//
+//  Created by Jarrale Butts on 10/17/19.
+//  Copyright © 2019 Jarrale Butts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
